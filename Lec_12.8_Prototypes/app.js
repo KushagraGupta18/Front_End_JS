01cea66aa5fb93a1ad4cf5e0ad43377491a42454
KushagraGupta18 <kushagragpt2003@gmail.com>
2024-02-09 19:18:46 +0530

    commit

--- Lec_12.8_Prototypes/app.js
+++ Lec_12.8_Prototypes/app.js
@@ -1,15 +1,61 @@
 
 
 
-const person = {
-    name: 'Kartik',
-    age: 25,
-    isAdult: true,
-    sayHello: function () {
-        console.log('Hello from Kartik');
+// const person = {
+//     name: 'Kartik',
+//     age: 25,
+//     isAdult: true,
+//     sayHello: function () {
+//         console.log('Hello from Kartik');
+//     }
+// }
+
+
+// let p1 = Object.create(person);
+// let p2 = Object.create(p1);
+
+
+
+
+
+
+//sam
+let todo={
+    title:'buy flowers',
+    desc:function(){
+        return `your task is to ${this.title}`
     }
 }
+console.log(todo.title);
+console.log(todo.desc());
+console.log(todo.toString());//if we console todo.toString() it will give us object this is only because of prototype 
+
+//if you try to access the property of an object than javascript will firstly find it inside that object , than if it not found , it will go to its prototype.
 
 
-let p1 = Object.create(person);
-let p2 = Object.create(p1);
+//what is protoptype---> it is also an object which is used as a fallback source(jb aapka project fail ho jata hai tb vo fallback krta hai prototypes ke pass) of properties.
+
+//object
+//in above case toString() is not a part of todo object than it goes to its prototype.
+//    __proto__    (--->dunder proto)  (dunder proto gives us parent of the object)
+//todo.__proto__ is equals to Object.prototype(fallback source of object todo)
+//parent of object.prototype is null.(object.prototype.__proto__ is equals to null)
+
+
+
+//array
+//in the case of an array if we declare array it will convert into an object.
+//and the prototype of an array is Array.prototype(arr.__proto is equal to Array.prototype)
+//and the prototype of an Array.prototype is Object.prototype and prototype of Object.prototype is null
+//arr.__proto gives us all methods of an array
+
+
+
+//string 
+//string behind the scene converts into an object.
+// and the prototype of an string is String.prototype(str.__proto is equal to String.prototype)
+//and the prototype of an String.prototype is Object.prototype and prototype of Object.prototype is null
+//str.__proto gives us all methods of an String.
+
+//same as boolean same as function
+ 
--- Lec_12.9_Constructor_Format/app.js
+++ Lec_12.9_Constructor_Format/app.js
@@ -9,15 +9,71 @@
 // fun('Kartik',25);
 
 
-function Person(name, age) {
-    this.name = name;
-    this.age = age;
-}
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
 
-Person.prototype.sayHello=function() {
-    console.log(`Hello from ${this.name}`);
-}
+// Person.prototype.sayHello=function() {
+//     console.log(`Hello from ${this.name}`);
+// }
 
-const p1 = new Person('Vivek', 23);
-const p2 = new Person('Garvit', 24);
-const p3 = new Person('Tushar', 22);
+// const p1 = new Person('Vivek', 23);
+// const p2 = new Person('Garvit', 24);
+// const p3 = new Person('Tushar', 22);
+
+
+
+
+
+//sam
+// function fun(){
+
+// }
+// let naam=fun();
+// console.log(naam);//undefined
+
+
+//constructor(new keyword before the function calling is creating this function as a constructor function whose task / job is to create object.)
+// function user(){
+//     this.username='kush',
+//     this.email='kush@gmail.com'
+// }
+// let user1=new user();//create object
+// console.log(user1);
+
+
+//Prototype of a User function is User that is made by you
+// function User(username,email){
+//     this.username=username,
+//     this.email=email,
+//     this.desc=function(){
+//         return `my name is ${this.username}`
+//     }
+// }
+// let user1=new User('kush','kush@gmail.com');//create object
+// console.log(user1);
+// console.log(user1.desc());
+// let user2=new User('vishu','vishu@gmail.com');//create object
+// console.log(user2);
+// console.log(user2.desc());
+
+//i always make constructor function by Capital letter
+
+
+
+function User(username,email){
+    this.username=username,
+    this.email=email
+}
+User.prototype.desc=function(){ //i add description inside the prototype
+    return `my name is ${this.username}`;
+}
+let user1=new User('kush','kush@gmail.com');//create object
+console.log(user1);
+console.log(user1.desc());
+let user2=new User('vishu','vishu@gmail.com');//create object
+console.log(user2);
+console.log(user2.desc());
+
+
